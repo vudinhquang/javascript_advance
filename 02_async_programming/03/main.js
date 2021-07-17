139cf3c72b560dee96e668ae68209ede94e13b6b
vudinhquang <vudinhquangk53@gmail.com>
2021-07-17 21:51:55 +0900

    Event Loop và Callback Queues

--- 02_async_programming/03/main.js
+++ 02_async_programming/03/main.js
@@ -4,6 +4,21 @@ function logHelloWorld() {
   console.log('3. Hello World');
 }
 
-setTimeout(logHelloWorld, -1000);
+setTimeout(logHelloWorld, 1000);
+
+function block3Seconds() {
+  const start = new Date().getTime();
+
+  while (true) {
+    const end = new Date().getTime();
+
+    if (end - start > 3000) {
+      console.log('end loop');
+      break;
+    } 
+  }
+}
+
+block3Seconds();
 
 console.log('2. Hello Javascript');
