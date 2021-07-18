4a3ce2b8514ba4c46ab7eb0ac2018d59681d61a8
vudinhquang <vudinhquangk53@gmail.com>
2021-07-19 00:01:01 +0900

    Promise và Patterns

--- 03_promise/02/main.js
+++ 03_promise/02/main.js
@@ -1,5 +1,25 @@
 // API Github: https://api.github.com/users
 
-const promiseObj = fetch('https://api.github.com/users');
+const promiseObj = fetch('https://api.github.commm');
+// const promiseObj = fetch('https://api.github.com/users');
 
-console.log(promiseObj);
+/*
+promiseObj.then(
+  (data) => {
+    console.log('data', data);
+  }, 
+  (error) => {
+    console.log('error', error);
+  }
+)
+*/
+
+promiseObj.then(
+  (data) => {
+    console.log('data', data);
+  }
+).catch(
+  (error) => {
+    console.log('error', error);
+  }
+)
