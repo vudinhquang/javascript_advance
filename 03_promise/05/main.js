7dcf1602fda1cb9757d96d23830bac1560fe5cb6
vudinhquang <vudinhquangk53@gmail.com>
2021-07-20 23:44:52 +0900

    Promise Chain

---
+++ 03_promise/05/main.js
@@ -0,0 +1,19 @@
+fetch('https://api.github.com/users/1')
+  .then(res1 => {
+    console.log('res1', res1);
+    return fetch('https://api.github.com/users/2');
+  })
+  .then(res2 => {
+    console.log('res2', res2);
+    return fetch('https://api.github.com/users/3');
+  })
+  .then(res3 => {
+    console.log('res3', res3);
+    return 20;
+  })
+  .then(res4 => {
+    console.log('res4', res4);
+  })
+  .catch(err => {
+    console.log('err', err);
+  });
