7526db529e4d56737ccb76359e2644478cac00c6
vudinhquang <vudinhquangk53@gmail.com>
2021-07-24 15:40:55 +0900

    Async Await

---
+++ 04_iterators_generator_async-await/08/main.js
@@ -0,0 +1,48 @@
+function fakeGetData(url) {
+	var fakeResponses = {
+		"https://quangvu.com/api/1": "Data1",
+		"https://quangvu.com/api/2": "Data2",
+		"https://quangvu.com/api/3": "Data3"
+	};
+	var randomMiliseconds = Math.floor(Math.random() * 5000) + 1000;
+
+	console.log(`Đang gọi API với URL : ${url} - Thời gian ${randomMiliseconds}ms`);
+
+	return new Promise((successFn, errorFn) => {
+    setTimeout(function(){
+      successFn(fakeResponses[url]);
+      // errorFn(fakeResponses[url]);
+    }, randomMiliseconds);
+  })
+}
+
+/* Tự setup Generator và Promise
+function* handleData() {
+  console.log('1. Truoc khi goi API');
+  const response = yield fakeGetData('https://quangvu.com/api/1')
+  console.log('2. Sau khi goi API');
+  console.log('3. Data sau khi goi API', response);
+}
+
+
+const genObj = handleData();
+genObj.next()
+  .value.then(function(result) {
+    genObj.next(result)
+  })
+*/
+
+async function handleData() {
+  try {
+    console.log('1. Truoc khi goi API');
+    const response = await fakeGetData('https://quangvu.com/api/1')
+    console.log('2. Sau khi goi API');
+    console.log('3. Data sau khi goi API', response);
+    
+    console.log('response', response);
+  } catch(err) {
+    console.error('err', err);
+  }
+}
+
+handleData()
