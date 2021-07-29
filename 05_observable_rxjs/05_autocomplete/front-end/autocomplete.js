712eca83a1c9abf68253a0c7711ad77e21a4a425
vudinhquang <vudinhquangk53@gmail.com>
2021-07-29 23:58:35 +0900

    Stream data

--- 05_observable_rxjs/05_autocomplete/front-end/autocomplete.js
+++ 05_observable_rxjs/05_autocomplete/front-end/autocomplete.js
@@ -1,8 +1,22 @@
+function makeURLSearch(str) {
+    return "http://localhost:8888/data?search=" + str;
+}
 
-var searchStr = 'nur'
-var url = 'http://localhost:8888/data?search=' + searchStr
+function autoComplete() {
+    const inputEl = document.querySelector("#js-input-search");
 
-Observable.fetch(url)
-    .forEach(data => {
-        console.log(data);
-    })
+    Observable.fromEvent(inputEl, "input")
+        .map(evt => evt.target.value)
+        .debounceTime(400)
+        .filter((searchStr) => searchStr.length >= 2)
+        .map((searchStr) => {
+            queryStr = searchStr
+            return makeURLSearch(searchStr)
+        })
+        .map((url) => Observable.fetch(url))
+        .forEach(data => {
+            console.log("data", data)
+        })
+}
+
+autoComplete()
