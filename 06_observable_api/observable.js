ecde9618b7daa36cdf3366b976fe409ce873cdc7
vudinhquang <vudinhquangk53@gmail.com>
2021-07-31 02:15:40 +0900

    Observable Class 02

--- 06_observable_api/observable.js
+++ 06_observable_api/observable.js
@@ -5,15 +5,70 @@ class Observable {
 
     static timeout(miliseconds) {
   
-        function subscribe(observer) {
+        function subscribe(next, error, complete) {
+            let observer = null
+
+            if (typeof next === 'function') {
+                observer = {
+                    next: next,
+                    error: error || function() {},
+                    complete: complete || function() {}
+                }
+            } else {
+                observer = next
+            
+                if (!observer.error) {
+                    observer.error = function() {}
+                }
+                if (!observer.complete) {
+                    observer.complete = function() {}
+                }
+            }
+
             const timeoutId = setTimeout(() => {
-                observer.next(miliseconds)
+                observer.next()
                 observer.complete()
             }, miliseconds);
         
             return {
                 unsubscribe() {
                     clearTimeout(timeoutId)
+                }
+            }
+        }
+    
+        return new Observable(subscribe)
+    }
+
+    static interval(miliseconds) {
+  
+        function subscribe(next, error, complete) {
+            let observer = null
+
+            if (typeof next === 'function') {
+                observer = {
+                    next: next,
+                    error: error || function() {},
+                    complete: complete || function() {}
+                }
+            } else {
+                observer = next
+            
+                if (!observer.error) {
+                    observer.error = function() {}
+                }
+                if (!observer.complete) {
+                    observer.complete = function() {}
+                }
+            }
+
+            const intervalId = setInterval(() => {
+                observer.next()
+            }, miliseconds);
+        
+            return {
+                unsubscribe() {
+                    clearInterval(intervalId)
                 }
             }
         }
