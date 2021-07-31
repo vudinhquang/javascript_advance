ef3e0136cd87bc29ac7502a565ccb9a34c610729
vudinhquang <vudinhquangk53@gmail.com>
2021-08-01 02:01:57 +0900

    RxJs Version 5.x

---
+++ 07_rxjs_v5/main.js
@@ -0,0 +1,60 @@
+const Observable = window.Rx.Observable
+
+
+const boxEl = document.querySelector('#box')
+// function: next, err, complete
+/*
+Observable
+  .fromEvent(boxEl, 'mousemove')
+  .map(evt => {
+    return {
+      offsetX: evt.offsetX,
+      offsetY: evt.offsetY
+    }
+  })
+  .filter(evt => evt.offsetX >= 150)
+  .take(10)
+  .subscribe(
+    (evt) => {
+      console.log('evt', evt)
+    },
+    null,
+    () => {
+      console.log('complete')
+    }
+  )
+*/
+// object {next, err, complete}
+/*
+Observable
+  .fromEvent(boxEl, 'mousemove')
+  .map(evt => {
+    return {
+      offsetX: evt.offsetX,
+      offsetY: evt.offsetY
+    }
+  })
+  .filter(evt => evt.offsetX >= 150)
+  .take(10)
+  .subscribe({
+      next: (evt) => {
+        console.log('evt', evt)
+      },
+      complete: () => {
+        console.log('complete')
+      }
+    }
+  )
+*/
+
+// interval
+let count = 0;
+Observable
+  .interval(1000)
+  .take(5)
+  .subscribe({
+    next: () => {
+      count++
+      console.log('run ', count)
+    }
+  })
