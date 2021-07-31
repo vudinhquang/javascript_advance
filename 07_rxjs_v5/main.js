c961f1539a52e118de08bc14cc64a4e1f7c9fa89
vudinhquang <vudinhquangk53@gmail.com>
2021-08-01 02:15:30 +0900

    ConcatAll

--- 07_rxjs_v5/main.js
+++ 07_rxjs_v5/main.js
@@ -48,10 +48,30 @@ Observable
 */
 
 // interval
+/*
 let count = 0;
 Observable
   .interval(1000)
   .take(5)
+  .subscribe({
+    next: () => {
+      count++
+      console.log('run ', count)
+    }
+  })
+*/
+
+// ConcatAll
+let count = 0;
+Observable
+  .interval(1000)
+  .take(5)
+  .map(() => {
+    return Observable
+      .interval(500)
+      .take(5)
+  })
+  .concatAll()
   .subscribe({
     next: () => {
       count++
